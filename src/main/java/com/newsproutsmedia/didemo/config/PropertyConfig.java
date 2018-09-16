fbf4926d4cdf2c33568eb7a4eb7b373ea70779e8
Nate Walters <newsproutsmedia@gmail.com>
2018-09-16 13:49:57 -0700

    Finished spring configuration module

--- src/main/java/com/newsproutsmedia/didemo/config/PropertyConfig.java
+++ src/main/java/com/newsproutsmedia/didemo/config/PropertyConfig.java
@@ -2,29 +2,24 @@ package com.newsproutsmedia.didemo.config;
 
 import com.newsproutsmedia.didemo.examplebeans.FakeDataSource;
 import com.newsproutsmedia.didemo.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
 //@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
 //or, for multiple property sources, you can use
-@PropertySources({
-        @PropertySource("classpath:datasource:properties"),
-        @PropertySource("classpath:jms.properties")
-})
+//@PropertySources({
+//        @PropertySource("classpath:datasource:properties"),
+//        @PropertySource("classpath:jms.properties")
+//})
 // either is acceptable, but for large numbers of property files, @PropertySources is easier to read
 public class PropertyConfig {
 
     // Pull override data from Environment Variables
     // Those are set up in "Edit Configurations"/Environment screen
-    @Autowired
-    Environment env;
+//    @Autowired
+//    Environment env;
 
     @Value("${com.username}")
     String user;
@@ -47,7 +42,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        //fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -63,10 +59,10 @@ public class PropertyConfig {
     }
 
     // reads and scans for properties file listed in @PropertySource annotation at top
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer properties() {
+//        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+//        return propertySourcesPlaceholderConfigurer;
+//    }
 
 }
